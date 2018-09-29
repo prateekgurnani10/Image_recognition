9ec16516b5116a73611541627f6c445bab013fe8
Holden.B <holden.bab@outlook.com>
2018-09-29 18:53:12 -0500

    Added cascades & added frontalface/eyes/smile detection

--- mainWindow.py
+++ mainWindow.py
@@ -1,8 +1,9 @@
 import sys
-
+from enum import Enum
 import cv2
+import qdarkstyle
 from PyQt5 import QtCore
-from PyQt5.QtCore import pyqtSlot, QDir
+from PyQt5.QtCore import pyqtSlot, QDir, QFile, QTextStream, pyqtSignal
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog
 from PyQt5.uic import loadUi
@@ -25,30 +26,45 @@ def my_exception_hook(exctype, value, traceback):
 sys.excepthook = my_exception_hook
 ####################################################################
 
-# TODO: Programmatically create behaviors such as contrast/brightness, when a behavior is added to the imageProcessor,
-# TODO: create a slider for it, and connect the slider to the behavior
-# TODO: image processor should contain an array or map of behaviors (can use an enum-like class to store strings)
+MAIN_WINDOW_DIALOG = 'main_window_dialog.ui'
 
 
-DIALOG_NAME = 'main_dialog.ui'
+class CascadeList(Enum):
+    EYE_CASCADE = 0
+    FACE_CASCADE = 1
+    SMILE_CASCADE = 2
+
+
+class Cascades:
+    def __init__(self):
+        self.cascades_list = {
+            CascadeList.EYE_CASCADE: cv2.CascadeClassifier("cascades/haarcascade_eye.xml"),
+            CascadeList.FACE_CASCADE: cv2.CascadeClassifier("cascades/haarcascade_frontalface.xml"),
+            CascadeList.SMILE_CASCADE: cv2.CascadeClassifier("cascades/haarcascade_smile.xml")
+        }
 
 
 class MainWindow(QDialog):
 
     def __init__(self):
         super(MainWindow, self).__init__()
-        loadUi(DIALOG_NAME, self)
+        loadUi(MAIN_WINDOW_DIALOG, self)
 
-        self._color_img = None
-        self._grayscale_img = None
+        self._cascades = Cascades()     # Mapping of cascades to their cascade classifiers
+        self._color_img = None          # Colored image
+        self._grayscale_img = None      # Grayscale image
+        self._processed_img = None      # Processed image
 
+        self.exitButton.clicked.connect(self.on_exit_button_clicked)
         self.importButton.clicked.connect(self.on_import_clicked)
         self.saveButton.clicked.connect(self.on_save_clicked)
-        self.cannyButton.clicked.connect(self.on_canny_clicked)
+        self.detectButton.clicked.connect(self.on_detect_clicked)
         self.cannySlider.valueChanged.connect(self.on_canny_slider_value_changed)
 
     @pyqtSlot()
     def on_canny_slider_value_changed(self):
+        # Always set slider text
+        self.cannySliderPositionLabel.setText(str(self.cannySlider.value()))
         if self._grayscale_img is None:
             return
 
@@ -57,33 +73,63 @@ class MainWindow(QDialog):
         self.display_img(canny_image, self.rightImgLabel)
 
     @pyqtSlot()
-    def on_canny_clicked(self):
+    def on_detect_clicked(self):
         if self._grayscale_img is None:
             return
 
-        # Preset canny image filter on button click
-        canny_image = cv2.Canny(self._grayscale_img, 100, 200)
-        self.display_img(canny_image, self.rightImgLabel)
+        # Detect faces
+        faces = self._cascades.cascades_list[CascadeList.FACE_CASCADE].detectMultiScale(self._grayscale_img, 1.3, 5)
+        for (x, y, w, h) in faces:
+            if self.detectFaceCheckBox.isChecked():
+                cv2.rectangle(self._processed_img, (x, y), (x + w, y + h), (255, 0, 0), 3)
+            else:
+                self._processed_img = self._color_img.copy()
+
+            roi_grayscale = self._grayscale_img[y: y + h, x: x + h]
+            roi_color = self._processed_img[y: y + h, x: x + h]
+
+            if self.detectEyesCheckBox.isChecked():
+                # Detect eyes
+                eyes = self._cascades.cascades_list[CascadeList.EYE_CASCADE].detectMultiScale(roi_grayscale, 1.1, 22)
+                for (ex, ey, ew, eh) in eyes:
+                    cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+            else:
+                self._processed_img[y: y + h, x: x + w] = self._color_img[y: y + h, x: x + w].copy()
+
+            if self.detectSmileCheckBox.isChecked():
+                smile = self._cascades.cascades_list[CascadeList.SMILE_CASCADE].detectMultiScale(roi_grayscale, 1.7, 18)
+                for (sx, sy, sw, sh) in smile:
+                    cv2.rectangle(roi_color, (sx, sy), (sx + sw, sy + sh), (0, 0, 255), 2)
+            else:
+                self._processed_img[y: y + h, x: x + w] = self._color_img[y: y + h, x: x + w].copy()
+
+        self.display_img(self._processed_img, self.rightImgLabel)
 
     @pyqtSlot()
     def on_import_clicked(self):
-        (filename, _) = QFileDialog.getOpenFileName(self, 'Open File', QDir.home().path(), "Image Files (*.png)")
+        (filename, _) = QFileDialog.getOpenFileName(self, 'Open File', QDir.home().path(), "Image Files (*.jpg)")
         if filename:
             self.load_image(filename)
 
     @pyqtSlot()
     def on_save_clicked(self):
-        (filename, _) = QFileDialog.getSaveFileName(self, 'Save File', QDir.home().path(), "Image Files (*.png)")
+        (filename, _) = QFileDialog.getSaveFileName(self, 'Save File', QDir.home().path(), "Image Files (*.jpg)")
         if filename:
-            cv2.imwrite(filename, self._color_img)
+            cv2.imwrite(filename, self._processed_img)
+
+    @pyqtSlot()
+    def on_exit_button_clicked(self):
+        self.close()
 
     def load_image(self, img_name):
         self._color_img = cv2.imread(img_name)
         self._grayscale_img = cv2.cvtColor(self._color_img, cv2.COLOR_BGR2GRAY)
+        self._processed_img = self._color_img.copy()
 
         if self._color_img is None:
             print(f"Invalid image path: {img_name}")
         else:
+            # Display the color image on the left panel first
             self.display_img(self._color_img, self.leftImgLabel)
 
     @staticmethod
@@ -109,9 +155,13 @@ class MainWindow(QDialog):
         image_label.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
 
 
+####################################################################
 def main():
     app = QApplication(sys.argv)
+
     window = MainWindow()
+    app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+    window.setWindowFlags(QtCore.Qt.FramelessWindowHint)
     window.setWindowTitle("Dejavu Interface")
     window.show()
 
@@ -121,5 +171,6 @@ def main():
         print("Error")
 
 
+####################################################################
 if __name__ == '__main__':
     main()
